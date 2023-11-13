85f1aeb235ad483c75fe7fbd8eddde98e2f4dcf7
Kelvin Sapathy <71108701+k-path@users.noreply.github.com>
2023-11-12 23:15:02 -0500

    Added execute code more safely, logging, measuring execution time
    Replaced direct code execution with a simulated process. Better for security, especially when we implement actual code execution.  
    
    Basic logging to track code execution requests.
    
    Measure the time taken for the (simulated) execution of the code.

--- src/python_interface/app.py
+++ src/python_interface/app.py
@@ -1,29 +1,33 @@
 from flask import Flask, render_template, request
+import logging
+import time
+import subprocess
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+def run_custom_compiler(code):
+    # For the purpose of this demonstration, we will simulate code execution
+    # In a real-world scenario, consider using a safe execution environment
+    logging.info("Executing code")
+    start_time = time.time()
+    
+    # Simulated code execution (replace with actual execution logic)
+    time.sleep(1)  # Simulating delay
+    output = "Simulated Output: " + code
+    
+    execution_time = time.time() - start_time
+    return output, execution_time
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         code = request.form['code']
-        output = run_custom_compiler(code) 
-        return render_template('index.html', code=code, output=output)
-    return render_template('index.html', code='', output='')
+        output, execution_time = run_custom_compiler(code)
+        return render_template('index.html', code=code, output=output, execution_time=execution_time)
+    return render_template('index.html', code='', output='', execution_time=0)
 
-def run_custom_compiler(code):
-    # Dummy function
-    return "Output" + code 
-
-def selectExample(value):
-    if value == "helloWorld":
-        code = 'std::cout << "Hello World!";'
-    elif value == 'simpleLoop':
-        code = 'if (int i = 0; i < 10; i++) { std::cout << i << std::endl; }'
-    elif value == 'bubbleSort':
-        code = 'int arr[] = {2, 5, 1, 8, 5, 6, 3}; int n = sizeof(arr)/sizeof(int); int i, j; for (i = 0; i < n - 1; i++) { for (j = 0; j < n - i - 1; j++) { if (arr[j] > arr[j + 1]) { swap(arr[j], arr[j + 1]); } } }'
-    elif value == 'findFactorial':
-        code = 'int factorial(int n) { if (n > 1) { return n * factorial(n - 1) } else { return 1; }} int main() { int n = 10; cout << factorial(n); } }'
-    return render_template('index.html', code=code, output=code)
-            
 if __name__ == '__main__':
     app.run(debug=True)
