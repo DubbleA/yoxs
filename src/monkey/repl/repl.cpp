9b6f93a3d9e7061ff941d334466bed83bb3d2c9d
yams <yams@nyu.edu>
2023-11-16 15:29:42 -0500

    made repl more verbose

--- src/monkey/repl/repl.cpp
+++ src/monkey/repl/repl.cpp
@@ -68,37 +68,52 @@ void REPL::Start(std::istream& in, std::ostream& out) {
     }
 }
 
-void REPL::StartSingle(std::istream& in, std::ostream& out){
+void REPL::StartSingle(std::istream& in, std::ostream& out) {
     std::string line;
-    
+
     out << PROMPT;
     if (!std::getline(in, line)) {
         return; // Exit if there's an error or EOF is encountered
     }
+
     out << "Input: " << line << "\n";
-    Lexer l(line);
+
+    // Lexical Analysis
+    out << "Starting Lexical Analysis...\n";
     Lexer lPrint(line);
     out << "Tokens:\n";
     for (Token tok = lPrint.NextToken(); tok.Type != TokenType::EOF_TOKEN; tok = lPrint.NextToken()) {
-        out << TokenTypeToString(tok.Type) << ": " << tok.Literal << "\n";
+        out << "  " << TokenTypeToString(tok.Type) << ": '" << tok.Literal << "'\n";
+        // Add more details here if needed, like line and character position
     }
+
+    // Parsing
+    out << "\nStarting Parsing...\n";
+    Lexer l(line);
     Parser p(l);
-        
     auto program = p.ParseProgram();
     if (!p.Errors().empty()) {
         printParserErrors(out, p.Errors());
+        return; // Stop further processing if there are parsing errors
     }
+    out << "Parsed Program (AST):\n  " << program->String() << "\n";
 
-    out << "Program: " << program->String() << "\n";
-
+    // Evaluation
+    out << "\nStarting Evaluation...\n";
     auto env = std::make_shared<Environment>();
     Evaluator evaluator;
     auto evaluated = evaluator.Eval(program, env);
+
+    // Displaying the environment state could be added here
+
     if(evaluated) {
-        out << "Evaluated: " << evaluated->Inspect() << "\n";
+        out << "Evaluated Result: " << evaluated->Inspect() << "\n";
+    } else {
+        out << "No output from evaluation.\n";
     }
-    
+
 }
+
 
 void REPL::printParserErrors(std::ostream& out, const std::vector<std::string>& errors) {
     out << "Woops! We ran into an error:\n";
