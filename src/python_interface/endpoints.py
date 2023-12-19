8b465e7d150662cc1f8d7bdb75c857fffc5505c7
yams <yams@nyu.edu>
2023-12-18 20:54:47 -0500

    added docstrings to endpoints

--- src/python_interface/endpoints.py
+++ src/python_interface/endpoints.py
@@ -54,11 +54,26 @@ class HelloWorld(Resource):
 @ns.route('/db_content')
 class CodeList(Resource):
 
-#ENDPOINT #1 Listing all Sample Programs
+#ENDPOINT #1 Listing all Sample Programs    
+    """
+    CodeList Endpoint
 
+    This endpoint is used for operations related to code snippets stored in the database.
+    It allows for listing all available code snippets and adding new ones.
+    """
     @ns.doc('list_codes', description='List all codes')  # Custom description for GET 
     @ns.marshal_list_with(code_model)
     def get(self):
+        """
+        List All Codes
+
+        Retrieves a list of all code snippets stored in the database.
+        This can be used to display all available samples to the user.
+
+        Responses:
+            200: Success
+            404: Not Found (if no codes are available)
+        """
         code_samples = mongo.db.samples.find()
         return list(code_samples), 200
 
@@ -67,18 +82,45 @@ class CodeList(Resource):
     @ns.doc('create_code', description='Create a new code')  # Custom description for POST
     @ns.expect(code_model)
     def post(self):
+        """
+        Create a New Code
+
+        Allows for adding a new code snippet to the database.
+        The code data should be provided in the request body.
+
+        Responses:
+            201: Created
+            400: Bad Request (if data is missing or invalid)
+        """
         code_data = request.json
         mongo.db.samples.insert_one(code_data)
         return {'result': 'code added'}, 201
 
 @ns.route('/id_by_name')
 class CodeIdByName(Resource):
+    """
+    CodeIdByName Endpoint
 
+    This endpoint retrieves the MongoDB ObjectID of a code sample based on its name.
+    """
 #ENDPOINT #3 Get mongoDB objectID by sample program name
 
     @ns.doc('get_id_by_name')
     @ns.param('name', 'The name of the sample program')
     def get(self):
+        """
+        Get ID by Name
+
+        Fetches the MongoDB ObjectID for a given code sample name.
+
+        Query Parameters:
+            name: The name of the code sample.
+
+        Responses:
+            200: Success - Returns the ObjectID of the code sample.
+            400: Bad Request - If the name parameter is missing.
+            404: Not Found - If no code sample with the given name is found.
+        """
         name = request.args.get('name')
         if not name:
             api.abort(400, 'A program name is required')
@@ -93,12 +135,29 @@ class CodeIdByName(Resource):
 @ns.response(404, 'Sample not found')
 @ns.param('sample_id', 'The sample identifier')
 class Code(Resource):
+    """
+    Code Endpoint
 
+    This endpoint allows fetching and deleting a specific code sample using its MongoDB ObjectID.
+    """
 #ENDPOINT #4 Get sample program name by MongoDB ObjectID
 
     @ns.doc('get_code', description='Get a specific code')  # Custom description for GET with ID
     @ns.marshal_with(code_model)
     def get(self, sample_id):
+        """
+        Get Code by ID
+
+        Retrieves a specific code sample based on its MongoDB ObjectID.
+
+        Path Parameters:
+            sample_id: The MongoDB ObjectID of the code sample.
+
+        Responses:
+            200: Success - Returns the requested code sample.
+            400: Bad Request - If the ObjectID is invalid.
+            404: Not Found - If no code sample with the given ID is found.
+        """
         try:
             oid = ObjectId(sample_id)
         except:
@@ -113,6 +172,19 @@ class Code(Resource):
 
     @ns.doc('delete_code', description='Delete a specific code')  # Custom description for DELETE
     def delete(self, sample_id):
+        """
+        Delete Code by ID
+
+        Deletes a specific code sample based on its MongoDB ObjectID.
+
+        Path Parameters:
+            sample_id: The MongoDB ObjectID of the code sample to delete.
+
+        Responses:
+            204: No Content - Code sample successfully deleted.
+            400: Bad Request - If the ObjectID is invalid.
+            404: Not Found - If no code sample with the given ID is found.
+        """
         try:
             oid = ObjectId(sample_id)
             return {'result': 'code deleted'}, 204
@@ -128,10 +200,27 @@ class Code(Resource):
 
 @ns.route('/compile')
 class CompileCode(Resource):
+    """
+    CompileCode Endpoint
+
+    This endpoint compiles and executes the provided code snippet and returns the output.
+    """
     @ns.doc('compile_code')
     @ns.expect(compile_model)
     @ns.marshal_with(compile_response_model)
     def post(self):
+        """
+        Compile and Execute Code
+
+        Compiles and executes a provided code snippet. The source code must be included in the request body.
+
+        Request Body:
+            code: The source code to compile and execute.
+
+        Responses:
+            200: Success - Returns the output of the compiled code and execution time.
+            400: Bad Request - If no code is provided or compilation fails.
+        """
         code = request.json.get('code')
         if not code:
             api.abort(400, "No code provided")
@@ -143,8 +232,22 @@ class CompileCode(Resource):
 
 @ns.route('/total_samples')
 class TotalSamples(Resource):
+    """
+    TotalSamples Endpoint
+
+    This endpoint returns the total number of code samples available in the database.
+    """
+
     @ns.doc('get_total_samples', description='Get total number of sample programs')
     def get(self):
+        """
+        Get Total Samples
+
+        Fetches the total count of code samples stored in the database.
+
+        Responses:
+            200: Success - Returns the total count of code samples.
+        """
         count = mongo.db.samples.count_documents({})
         return {'total_samples': count}
 
@@ -154,9 +257,31 @@ class TotalSamples(Resource):
 @ns.response(404, 'Sample not found')
 @ns.param('sample_id', 'The sample identifier')
 class UpdateCode(Resource):
+    """
+    UpdateCode Endpoint
+
+    This endpoint updates the details of a specific code sample identified by its MongoDB ObjectID.
+    """
+
     @ns.doc('update_code', description='Update a specific code')
     @ns.expect(code_model)
     def put(self, sample_id):
+        """
+        Update Code by ID
+
+        Modifies an existing code sample using its MongoDB ObjectID. The updated data should be provided in the request body.
+
+        Path Parameters:
+            sample_id: The MongoDB ObjectID of the code sample to update.
+
+        Request Body:
+            Updated details of the code sample.
+
+        Responses:
+            200: Success - Code sample successfully updated.
+            400: Bad Request - If the ObjectID is invalid or data is missing.
+            404: Not Found - If no code sample with the given ID is found.
+        """
         try:
             oid = ObjectId(sample_id)
         except:
@@ -174,9 +299,25 @@ class UpdateCode(Resource):
 
 @ns.route('/search')
 class SearchSamples(Resource):
+    """
+    SearchSamples Endpoint
+
+    This endpoint allows searching for code samples in the database using a keyword.
+    """
     @ns.doc('search_samples', description='Search sample programs by keyword')
     @ns.param('keyword', 'Keyword to search for')
     def get(self):
+        """
+        Search Samples by Keyword
+
+        Searches for code samples that match a given keyword in their names.
+
+        Query Parameters:
+            keyword: The keyword to search for in code sample names.
+
+        Responses:
+            200: Success - Returns a list of code samples matching the keyword.
+        """
         keyword = request.args.get('keyword', '')
         query = {"name": {"$regex": keyword, "$options": "i"}}  # Case-insensitive search
         samples = mongo.db.samples.find(query)
@@ -194,10 +335,27 @@ class SearchSamples(Resource):
 
 @ns.route('/list_by_page')
 class ListByPage(Resource):
+    """
+    ListByPage Endpoint
+
+    This endpoint lists code samples in a paginated format, allowing users to browse through samples in chunks.
+    """
     @ns.doc('list_by_page', description='List sample programs by page')
     @ns.param('page', 'Page number', _in='query', type='int')
     @ns.param('limit', 'Number of items per page', _in='query', type='int')
     def get(self):
+        """
+        List Samples by Page
+
+        Retrieves a specific page of code samples with a set limit on the number of items per page.
+
+        Query Parameters:
+            page: The page number to retrieve.
+            limit: The maximum number of items per page.
+
+        Responses:
+            200: Success - Returns a paginated list of code samples.
+        """
         page = int(request.args.get('page', 1))
         limit = int(request.args.get('limit', 10))
         skip = (page - 1) * limit
@@ -218,8 +376,22 @@ import random
 
 @ns.route('/random_sample')
 class RandomSample(Resource):
+    """
+    RandomSample Endpoint
+
+    This endpoint fetches a random code sample from the database.
+    """
     @ns.doc('get_random_sample', description='Get a random sample program')
     def get(self):
+        """
+        Get Random Sample
+
+        Retrieves a random code sample from the database.
+
+        Responses:
+            200: Success - Returns a random code sample.
+            404: Not Found - If no code samples are available.
+        """
         count = mongo.db.samples.count_documents({})
         if count == 0:
             return {'message': 'No samples available'}, 404
@@ -239,11 +411,35 @@ class RandomSample(Resource):
 
 @ns.route('/delete_all')
 class DeleteAllSamples(Resource):
+    """
+    DeleteAllSamples Endpoint
+
+    This endpoint deletes all code samples from the database.
+    """
     @ns.doc('delete_all_samples', description='Delete all sample programs')
     def delete(self):
+        """
+        Delete All Samples
+
+        Removes all code samples from the database.
+
+        Responses:
+            204: No Content - All code samples successfully deleted.
+        """
         mongo.db.samples.delete_many({})
         return {'result': 'all samples deleted'}, 204
+from datetime import datetime
 
+# New endpoint for getting the current server time
+@ns.route('/server_time')
+class ServerTime(Resource):
+    @ns.doc('get_server_time', description='Get the current server time')
+    def get(self):
+        """
+        Returns the current server time.
+        """
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return {'server_time': current_time}
 
 # Helper function
 def run_custom_compiler(code):
